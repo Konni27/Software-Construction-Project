681d352f56d87a34f3ea6ac6023bd2ee10e0508a
Konni27 <konradjoel27@gmail.com>
2026-03-15 16:29:00 +0000

    Book

--- src/main/java/is/hi/hbv202g/assignment8/Book.java
+++ src/main/java/is/hi/hbv202g/assignment8/Book.java
@@ -1,4 +1,50 @@
 package is.hi.hbv202g.assignment8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
+    private String title;
+    private List<Author> authors;
+
+    public Book(String title, String  authorName){
+        this.title = title;
+        this.authors = new ArrayList<>();
+        this.authors.add(new Author(authorName));
+    }
+
+    public Book(String title, List<Author> authors) throws EmptyAuthorListException{
+        if (authors == null || authors.isEmpty()){
+            throw new EmptyAuthorListException("Author list cannot be empty");
+        }
+        this.title = title;
+        this.authors = authors;
+
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) throws EmptyAuthorListException {
+        if (authors == null || authors.isEmpty()) {
+            throw new EmptyAuthorListException("Author list cannot be empty");
+        }
+        this.authors = authors;
+    }
+
+    public void addAuthors(Author author){
+        authors.add(author);
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
 }
